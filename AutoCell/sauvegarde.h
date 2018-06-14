1a6bf490e9f6c92314fcffad174e232e690fd238
Claire Guyot <36992869+claireguyot@users.noreply.github.com>
2018-06-14 22:48:20 +0200

    Documentation sauvegarde

--- AutoCell/sauvegarde.h
+++ AutoCell/sauvegarde.h
@@ -4,7 +4,7 @@
 
 /*!
  * \file sauvegarde.h
- * \brief Déclaration des méthodes liées à la sauvegarde du contexte d'un automate
+ * \brief Déclaration des fonctions liées à la sauvegarde du contexte d'un automate
  */
 
 
@@ -24,13 +24,36 @@
 
 class fichier;
 
-
+/**
+* \enum TypeFichier
+* \brief Type de sauvegarde de données
+*
+* Cette énumération défini le type de sauvegarde du fichier : soit un état, soit une configuration d'un automate 1D ou 2D.
+*/
 enum TypeFichier {ETAT, CONFIG};
 
+/**
+* \enum DimType
+* \brief Type de sauvegarde de données
+*
+* Cette énumération défini le type d'automate sauvegardé : soit 1D, soit 2D.
+*/
 enum DimType {_1D,_2D};
 
+
+
+/*!
+ * \brief Fonction liée à la sauvegarde des données d'un automate 1D ou 2D
+ *
+ * Cette fonction permet de sauvegarder l'état ou la configuration d'un automate dans un fichier \a .bn ou \a .csv, avec un nom saisi par l'utilisateur.
+ */
 bool sauvegarde(const CellularAutomata &automate, TypeFichier t, DimType d);
 
+/*!
+ * \brief Fonction liée au chargement des données d'un automate 1D ou 2D
+ *
+ * Cette fonction permet de charger l'état ou la configuration d'un automate à partir d'un fichier \a .bn ou \a .csv.
+ */
 bool chargement(CellularAutomata *&automate, TypeFichier t, DimType d);
 
 #endif // SAUVEGARDE_H
